a997dd2c89ca09e4b89cc4510eb30f53870fdab4
anas <ashiwani007@gmail.com>
2023-12-16 05:48:06 +0500

    WORK

---
+++ app.js
@@ -0,0 +1,551 @@
+// chap 1
+// q 1
+// alert ("First Name");
+// alert ("Last Name");
+// alert ("Email");
+// alert ("Phone Number");
+// alert ("Password");
+
+// q 2
+// alert ("You're learning JavaScript!");
+
+// q 3
+// alert ("My Name is Anas");
+
+// chap 2
+// q 1
+// var a = "anas";
+
+// q 2
+// var a ;
+// a = "anas";
+
+// q 3
+// var teamName = "pakistan";
+// alert (teamName);
+
+// q 4
+// var bestMan = "Charlie";
+// bestMan = "anas";
+// alert (bestMan);
+
+// chap 3
+// q 1
+// var caseQty;
+
+// q2
+// caseQty = 144;
+
+// q 3
+// var num = 9;
+
+// q 4
+// var sum;
+// sum = 2 + 2;
+// alert (sum);
+
+// q 5
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert (orderTotal);
+
+// q 6 
+// var num1 = 60;
+// num1 = 60 + 60;
+// alert (num1);
+
+// chap 4
+// q 1
+// var productCost = 3.45;
+
+// q 2
+// var nameOfBand;
+
+// q 3 
+// var legallyNamed = 23;
+
+// q 4
+// var anasShiwani ;
+// or 
+// var anasShiwani = 23;
+
+// q 5
+// FOR ILLEGAL
+// var 1stPlace;  // starts with a digit
+// var my variable;  // contains a space
+// var total#Amount;  // contains a special character
+// var var;  // reserved keyword
+// FOR LEGAL
+// var age;
+// var _firstName;
+// var $totalAmount;
+// var camelCaseVariable;
+
+// chap 5
+// q 1
+// var remainder = 10 % 3; 
+
+// q 2
+// var num = 20 % 6;
+// alert (num);
+// the answer of this question is 2
+
+// q 3
+// var largeNum = 1000 * 2000;
+// alert (largeNum);
+
+// q 4 
+// var a = 5;
+// var b = 3;
+// var c = a - b;
+// alert (c);
+
+// q 5
+// var remainder = 15 % 4;
+
+// q 6
+// var a = 2 * 2;
+// alert (a);
+
+
+// chap 6
+// q 1
+// var a = 1;
+// a++;
+// alert (a);
+ 
+// q 2
+// var x = 100;
+// x--;
+// alert (x);
+
+// q 3
+// var x = 50;
+// var y = x++;
+// alert (y);
+// 50 aye gi y ki value q k post increament hai
+// alert (x);
+// 51 aye gi x ki value q k post increament hai
+
+// q 4
+// var y = 50;
+// var z = --y;
+// alert (z);
+// 49 aye gi q k pre increament hai
+// alert (y);
+// 49 aye gi q k pre increament hai
+
+// q 5
+// var num = 10;
+// var newNum = --num;
+// alert (num);
+// alert (newNum);
+
+// q 6
+// var origanlValue = 5;
+// var newValue = origanlValue++;
+// alert (newValue);
+// alert (origanlValue);
+
+// q 7
+// var a = 12;
+// a++;
+// alert (a);
+
+// chap 7
+// q 1
+// var calculatedNum = 2 + (2 * 6);
+// alert (calculatedNum);
+
+// q 2
+// var calculatedNum = (2 + 2) * 6;
+// alert (calculatedNum);
+
+// q 3
+// var calculatedNum = (2 + 2) * (4 + 2);
+// alert (calculatedNum);
+
+// q 4
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// alert (calculatedNum);
+
+// q 5
+// var calculatedNum = (2 + 2 )*(4 + 10);
+// alert (calculatedNum);
+
+// q 6
+// var calculatedNum = 2 + (2 * 4) + 10;
+// alert (calculatedNum);
+
+// q 7
+// var pressure = 4 / (2 * 4);
+// alert (pressure);
+
+// chap 8
+// q 1
+// var num = "2" + "2";
+// console.log(num);  // Output: "22"
+
+// q 2 
+// var a = "hello " +  "World";
+// alert (a);
+
+// q 3
+// alert ("33"+3);
+
+// q 4
+// var part1 = "pakistan";
+// var part2 = "Zindabad";
+// alert (part1 + " "+ part2);
+
+// q 5
+// var result = "anas"+123;
+// alert (result);
+
+// q 6
+// var a = "anas";
+// var b = "rehman & moiz";
+// var c = a + " " + b;
+// alert (c);
+
+// chap 9
+// q 1 
+// var a = prompt("Enter First Name");
+
+//  q 2
+// var a = prompt ("Country?", "China");
+
+// // q 3
+// var yourName = prompt ("Enter Your Name ");
+
+// // q 4
+// var a = prompt ("Enter Your Name", "Anas");
+
+// q 5
+// var message = "Enter your name:";
+// var defaultResponse = "John Doe";
+// var userResponse = prompt (message, defaultResponse);
+
+// q 6
+// var a = prompt ("Enter Your Name", "Anas ?");
+// alert (`Hi ${a}!`);
+
+
+// chap 10 
+// q 1
+// var city = prompt("Enter Your City");
+// if (city == "karachi"){
+//     alert ("The City Of Light");
+// }
+// OR
+// var city = "karachi";
+// if (city == "karachi"){
+//     console.log ("the city of light");
+// }
+
+// q 2
+// var x = prompt("Enter Your City");
+// var y = prompt("Enter Your City");
+// if (x === y){
+//     var z = prompt ("Enter your Name");
+// }
+// OR
+// var x = "karachi";
+// var y = "karachi";
+// if (x === y){
+//     var z = prompt ("Enter Your Name")
+//     alert (`Hi ${z}!`)
+// }
+// OR
+// required answer
+//  var x = "karachi";
+//  var y = "karachi";
+//  if (x === y){
+//      var z = prompt ("Enter Your Name")
+     
+//  }
+
+// q 3
+// var zipCode = "10010";
+// if (zipCode == "10010"){
+//     alert ("karachi");
+// }else {
+//     alert ("Please write correct city");
+// }
+
+// q 4
+// var x = 1;
+// if (x === 1){
+//     x = 42;
+//     console.log (x)
+//     console.log (++x);
+//     console.log("updated value of x:" + x);
+// }
+
+
+// chap 15 {array 1}
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// question no 1
+// var arr = [];
+// console.log (arr);
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// question no 2
+// var arr = ["anas"];
+// console.log (arr);
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// ye question 3 k answer ko mene apni trf se lamba kya hai seekhne k liye
+// question no 3
+// var arr = ["h","j","k","l"];
+// var x = prompt("Enter the alphabet which you want to know are lies in the data ?")
+// if (arr.includes(x)){
+//     alert (`${arr.indexOf(x)} : this number is known as index number, this number placed on this index position. if the answer is -1 that's mean this alphabet is not lies in the array`);
+// }else if (!arr.includes(x)){
+//     alert ("not found");
+// }
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// question 3 actual answer
+// var arr = ["h","i","j","k","l"];
+// alert (arr[2]);
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// question 4
+// var alphabet=["h","i","j","k","l","m","n","o",];
+// var total = alphabet.length;
+// console.log (total);
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// var arr = ["a"];
+// arr[1] = "b";
+// alert (arr[1]);
+// console.log (arr);
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+
+// chap 16 {array 2}
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// question no 1
+// var arr = ["sir arslan"];
+// arr.push = "sir basit";
+// alert (arr.push);
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// question no 2
+// var Alphabet=["h","i","j","k"];
+// var removedElement = Alphabet.pop();
+// alert (removedElement);
+// alert me srf j sow kre ga 
+// console.log (Alphabet);
+// iss me j removed hojaye ga 
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// question no 3
+// var Alphabet=["h","i","j","k"];
+// Alphabet.push (42);
+// alert (Alphabet);
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// chap 16 {Array 3}
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// // question no 1
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// var removedElement = sizes.shift();
+// alert (removedElement);
+// console.log (sizes);
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// // // question no 2
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.unshift("p","q","r");
+// console.log (sizes);
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// // // // question no 3
+// var a = ["anas"];
+// var b = a.unshift("bilal");
+// alert (a[0]);
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// // // // // question no 4
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// var b = sizes.indexOf("XL");
+// console.log (b);
+// sizes.splice(b, 0, "L");
+// console.log (sizes);
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// chap 17 to 20 
+// nmbr 1
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// for (var i = 0 ; i < 10 ; i++){
+//     console.log (i);
+// }
+// nmbr 2
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// for (var i = 0 ; i <= 11 ; i++){
+//     console.log (i);
+// }
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// // nmbr 3
+// for (var i = 0 ; i < 10 ; i++){
+//     console.log (i);
+// }
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// nmbr 4
+// for (var counter = 0; counter < 100 ; counter++){
+//     console.log (counter);
+// }
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// nmbrc 5
+// for (var i = 3 ; i > 0 ; i--){
+//     console.log ("iteration" + " " + i);
+// }
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// nmbr 6
+// var array = [1, 2, 3, 4, 5];
+// var numberOfElements = array.length;
+
+// console.log(`The array has ${numberOfElements} elements.`);
+
+// nmbr 7 
+// var flag = true;
+// The variable flag is now a Boolean variable with the specified initial value
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// nmbr 8 
+// var pets = ["dog", "cat", "fish", "bird", "hamster"];
+// var numberOfPets = pets.length;
+// // console.log (numberOfPets);
+// for (i = 0; i < pets.length; i++){
+//     console.log(`Iteration ${i + 1}: ${pets[i]}`);
+// }
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// nmbrs 9
+// for (let i = 0; i < 10; i++) {
+//     if (i === 0) {
+//         alert(`iteration on ${i}`);
+//         // break;
+//     }else if (i === 1){
+//         alert (`iteration on ${i}`)
+//     }
+//     else {
+//         alert ("no value describe on this iteration, so the loop goes break");
+//         break;
+//     }
+// }
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+// nmbr 10
+// var arr = ["anas", "bilal", "zaid", "zain", "owais", "umer"];
+// var z = prompt("Enter first name");
+
+// for (i = 0; i < arr.length; i++) {
+//     if (z === arr[i]) {
+//         alert("existing");
+//         break;
+//     } else {
+//         alert("not existing");
+//     }
+// }
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// var userNames = ["anas", "bilal", "zaid", "zain"];
+// var x = prompt("Enter first name");
+// // for (var i = 0; i < userNames.length; i++) {
+// if (userNames.includes(x)) {
+//     // ye uper wali if line me agar x ko define na bh krien tw perform ho raha hai
+//     alert("existing");
+//     // break;
+// } else {
+//     alert("not existing");
+// }
+
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// nmbr 11
+// var matchFound = false;
+// var list = ["pakistan","india","australia","bangladesh"];
+// var userInput = prompt ("Type what you want to see ?");
+
+// for (var i = 0; i<list.length; i++){
+//     if (userInput === list[i]){
+//         alert ("Match Found");
+//     matchFound = true;
+//     break ;
+//     }
+// }; 
+// if (matchFound != true){
+//     alert ("match not found");
+// }
+// <<<<<<<<<<<<<<<<<_________>>>>>>>>>>>>>>>>>>>>></_________>
+
+// nmbr 12
+// var firstArr = ["a", "b", "c", "d", "e", "f"];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+// var combined = [];
+
+// for (i = 0; i < firstArr.length; i++){
+//     for (j = 0; j < secondArr.length; j++){
+//         combined.push(firstArr[i] + secondArr[j]);
+         
+//     }
+// }
+// console.log (combined);
+
+// ye uper waly method me combined use kya hai agr ek naya array bnana ho tw 
+// wrna jo given sawal ka solution hai oss ka ansnwer nichy hai
+
+// var firstArr = ["a", "b", "c", "d", "e", "f"];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+
+// for (i = 0; i < firstArr.length; i++){
+//     for (j = 0; j < secondArr.length; j++){
+//         console.log (firstArr[i] + secondArr[j]);
+         
+//     }
+// }
